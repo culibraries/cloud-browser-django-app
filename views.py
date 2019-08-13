00cbd835289f08717de7664f5b562e9e05c87372
dutr5288 <dutr5288@colorado.edu>
2019-08-13 15:41:29 -0600

    Fixed: EmptyObjectCreateView

--- views.py
+++ views.py
@@ -25,7 +25,7 @@ class BucketCreateView(APIView):
 
 class EmptyObjectCreateView(APIView):
     def get(self, request):
-        s3 = boto3.client('s3')
+        s3 = boto3.resource('s3')
         filename = request.GET.get('fname') + '/'
         s3.Bucket(request.GET.get('bname')).put_object(
             Key=filename, Body='', ACL='public-read')
