a11877026a72f3a53bc34933b4104cdfc0b6f86d
dutr5288 <dutr5288@colorado.edu>
2019-08-14 13:09:18 -0600

    Updated: url name

--- urls.py
+++ urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from s3.views import BucketListView, ObjectListView, BucketCreateView, ObjectCreateView, ObjectDeleteView, BucketDeleteView, ObjectUploadView
 
 urlpatterns = [
-    path('buckets', BucketListView.as_view()),
-    path('objects', ObjectListView.as_view()),
+    path('buckets', BucketListView.as_view(), name='buckets-list'),
+    path('objects', ObjectListView.as_view(), name='objects-list'),
     path('bucket/create', BucketCreateView.as_view()),
     path('bucket/delete', BucketDeleteView.as_view()),
     path('object/create', ObjectCreateView.as_view()),
     path('object/delete', ObjectDeleteView.as_view()),
-    path('object/upload', ObjectUploadView.as_view())
+    path('object/upload', ObjectUploadView.as_view(), name='object-upload')
 ]
