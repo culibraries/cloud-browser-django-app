be1ffeb420487ef279fc110298c76f1e56befaa6
Mark Stacy <mbstacy@gmail.com>
2021-10-26 13:38:08 -0600

    permissions debug

--- permissions.py
+++ permissions.py
@@ -9,18 +9,18 @@ class s3Permission(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        try:
-            groups=UserGroups(request).groups
-            bName = request.GET.get('bname')
-            special_case = os.getenv('UCB_ALL_BUCKETS', '').split(',')
-            groups=groups + special_case
+        #try:
+        groups=UserGroups(request).groups
+        bName = request.GET.get('bname')
+        special_case = os.getenv('UCB_ALL_BUCKETS', '').split(',')
+        groups=groups + special_case
 
-            if "{0}-rw".format(bName) in groups or "{0}-r".format(bName) in groups:
-                return True
-            else:
-                return False
-        except:
+        if "{0}-rw".format(bName) in groups or "{0}-r".format(bName) in groups:
+            return True
+        else:
             return False
+        # except:
+        #     return False
 
 class s3WritePermission(permissions.BasePermission):
     """
